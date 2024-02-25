d6301237966500fcd1ba8ab455912a159ae2d62c
Tom Arney <43818254+tomarney@users.noreply.github.com>
2024-02-25 18:29:34 +0000

    support maths in md and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math'
+import rehypeMathJax from 'rehype-mathjax'
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomarney.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax],
+  },
 });
